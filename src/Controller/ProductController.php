7f676c18a28722869a999bcfcf928c54d29e70d6
Leonard <adrianinformatica1920@gmail.com>
2024-10-28 09:29:39 +0100

    añadido producto y servicio producto

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Service\ProductService;
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'product')]
-    public function index(): Response
+    public function product(ProductService $productsService): Response
     {
-        return $this->render('product/product.html.twig', []);
+        $products = $productsService->getProducts();
+        return $this->render('product/product.html.twig', compact('products'));
     }
+
 }
